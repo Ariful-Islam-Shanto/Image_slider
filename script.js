9c1e2f404924ecd251e7fde14a49775557360fbf
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2024-03-10 01:38:09 +0600

    Added the final functionality of the slider text animation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sliders = document.querySelectorAll('.slider');
 const headers = document.querySelectorAll('.head');
+const visibleDiv = document.querySelectorAll('.visibleDiv');
 
 let counter = 0;
 console.log(counter);
@@ -7,17 +8,21 @@ sliders.forEach((slider, index) => {
     slider.style.left = `${index * 100}%`;
 })
 
+//? Slider next btn
 const slideNext = () => {
     if(counter > 4) return;
     counter++;
     slideImage()
 }
 
+
+//? Slider next btn
 const slidePrev = () => {
     counter--;
     slideImage()
 }
 
+//? Moving the slider. 
 function slideImage () {
     if(counter > 4) return;
     if(counter < 0) {
@@ -29,10 +34,6 @@ function slideImage () {
     if(counter === 4) {
         counter = 0;
         return  (
-            // sliders.forEach((slider, index) => {
-            //     slider.style.transform = `translateX(${index * 100}%)`
-            // }
-            // )
             sliders.forEach((slider, index) => {
                 slider.style.transform = `translateX(${0}%)`
             })
@@ -40,31 +41,37 @@ function slideImage () {
    }
 
    sliders.forEach((slider) => {
-    // if(index % 2 === 0) {
-    //     slider.style.transform = `translateY(${0}%)`
-    // }
     slider.style.transform = `translateX(-${counter * 100}%)`
 })
 }
 
 
 
-// const hides = document.querySelectorAll('.hide');
-
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
+        //? Targeting the children of the div that has been observed.
+        //? In this case. The "visibleDiv" is getting observed and the 
+        //? "head" element inside the "visibleDiv" is getting targeted.
+        const targetedElement = entry.target.querySelector('.head'); 
+        console.log(targetedElement);
+    
         if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-            entry.target.classList.remove('hide');
+            //? If the "visibleDiv" is in the viewport then we are
+            //? Adding the "visible" class to it's children "head"
+            targetedElement.classList.add('visible');
         } else {
-            entry.target.classList.remove('visible');
-            entry.target.classList.add('hide');
+            //? And here is vice-versa
+            targetedElement.classList.remove('visible');
         }
     });
 }, {threshold: 0});
+//? By giving threshold : 0 we are ensuring that the class won't be 
+//? Added to "head" element until the "visibleDiv" is fully on the viewport
 
-headers.forEach(head => {
-    observer.observe(head)
+
+//? In here observing each of the "visibleDiv" inside the every slide.
+visibleDiv.forEach(visibleDiv => {
+    observer.observe(visibleDiv)
 })
 
 console.log(headers);
